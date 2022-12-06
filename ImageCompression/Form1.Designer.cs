7a2359c2c1e2fe8a692755e5856e211cc3aab620
AbdullahAlatiyat <abdallahamzeh@hotmail.com>
2022-12-06 03:21:11 -0600

    initial

---
+++ ImageCompression/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace LoselessImageCompression
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label_outputPath = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label_filesNumber = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nmQualityCompressionLevel = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nmQualityCompressionLevel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(193, 117);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 28);
+            this.btnStart.TabIndex = 11;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(296, 50);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 28);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Select Output Path";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnSelectOutputPath_Click);
+            // 
+            // label_outputPath
+            // 
+            this.label_outputPath.AutoSize = true;
+            this.label_outputPath.Location = new System.Drawing.Point(20, 57);
+            this.label_outputPath.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_outputPath.Name = "label_outputPath";
+            this.label_outputPath.Size = new System.Drawing.Size(0, 16);
+            this.label_outputPath.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 86);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Select Files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnSelectFiles_Click);
+            // 
+            // label_filesNumber
+            // 
+            this.label_filesNumber.AutoSize = true;
+            this.label_filesNumber.Location = new System.Drawing.Point(20, 15);
+            this.label_filesNumber.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label_filesNumber.Name = "label_filesNumber";
+            this.label_filesNumber.Size = new System.Drawing.Size(25, 16);
+            this.label_filesNumber.TabIndex = 7;
+            this.label_filesNumber.Text = "0/0";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(89, 15);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(373, 28);
+            this.progressBar.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Quality Reduction";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(79, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "%";
+            // 
+            // nmQualityCompressionLevel
+            // 
+            this.nmQualityCompressionLevel.Location = new System.Drawing.Point(23, 121);
+            this.nmQualityCompressionLevel.Name = "nmQualityCompressionLevel";
+            this.nmQualityCompressionLevel.Size = new System.Drawing.Size(50, 22);
+            this.nmQualityCompressionLevel.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 160);
+            this.Controls.Add(this.nmQualityCompressionLevel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label_outputPath);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label_filesNumber);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image Compression";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nmQualityCompressionLevel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label_outputPath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_filesNumber;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown nmQualityCompressionLevel;
+    }
+}
+
---
+++ ImageCompression/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Encoder = System.Drawing.Imaging.Encoder;
+
+namespace LoselessImageCompression
+{
+    public partial class Form1 : Form
+    {
+        public static string[] files = null;
+        public static string[] allowedFileExtensions = { ".jpg", ".jpeg", ".png", ".gif", ".tif", ".tiff", ".bmp" };
+        public static int progressBarIncrement = 1;
+        public static int currentFileProcessing = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void resetEverything()
+        {
+            files = null;
+            progressBarIncrement = 1;
+            currentFileProcessing = 1;
+
+            label_filesNumber.Text = @"0\0";
+            label_outputPath.Text = @"";
+            progressBar.Value = 0;
+        }
+
+        private string getAllowedFileExtensions()
+        {
+            string result = "";
+            foreach (var item in allowedFileExtensions)
+            {
+                result += "*" + item + ";";
+            }
+            return result;
+        }
+
+        private void btnSelectFiles_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|" + getAllowedFileExtensions();
+                openFileDialog.RestoreDirectory = true;
+                openFileDialog.Multiselect = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if (openFileDialog.FileNames.Length == 0)
+                    {
+                        MessageBox.Show("you must choose atleast 1 file");
+                    }
+                    else
+                    {
+                        files = openFileDialog.FileNames;
+                        foreach (var item in files)
+                        {
+                            var extension = Path.GetExtension(item);
+                            if (!allowedFileExtensions.Contains(extension))
+                            {
+                                MessageBox.Show("1 or more files are not valid");
+                                break;
+                            }
+                        }
+
+                        label_filesNumber.Text = currentFileProcessing + "/" + files.Length;
+
+                        progressBarIncrement = 100 / files.Length;
+                        if (progressBarIncrement < 0)
+                            progressBarIncrement = 1;
+                    }
+                }
+            }
+        }
+
+        private bool ProcessFiles(int index)
+        {
+            try
+            {
+                var currentFile = files[index];
+                var fileName = Path.GetFileName(currentFile);
+
+                Bitmap myBitmap = new Bitmap(currentFile);
+
+                ImageCodecInfo myImageCodecInfo = GetEncoder(ImageFormat.Jpeg);
+
+                Encoder myEncoder = Encoder.Compression;
+                Encoder qualityEncoder = Encoder.Quality;
+
+                EncoderParameters myEncoderParameters = new EncoderParameters(2);
+
+                EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, (long)EncoderValue.CompressionLZW);
+                myEncoderParameters.Param[0] = myEncoderParameter;
+
+                EncoderParameter myEncoderParameterQuality = new EncoderParameter(qualityEncoder, 100 - long.Parse(nmQualityCompressionLevel.Text));
+                myEncoderParameters.Param[1] = myEncoderParameterQuality;
+
+                var outputFilePath = label_outputPath.Text + "\\out-" + fileName;
+                myBitmap.Save(outputFilePath, myImageCodecInfo, myEncoderParameters);
+
+                var oldFileInfo = new FileInfo(currentFile);
+                var newFileInfo = new FileInfo(outputFilePath);
+                if (oldFileInfo.Length < newFileInfo.Length)
+                {
+                    File.Delete(outputFilePath);
+                    File.Copy(currentFile, outputFilePath);
+                }
+
+                if (index != files.Length - 1)
+                {
+                    progressBar.Increment(progressBarIncrement);
+
+                    currentFileProcessing += 1;
+                    label_filesNumber.Text = currentFileProcessing.ToString() + "/" + files.Length.ToString();
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (ValidateForm())
+            {
+                var errorProcessingFilesMessage = "some files couldn't be processed:- " + Environment.NewLine;
+                var somethingWentWrongFlag = false;
+                for (int i = 0; i < files.Length; i++)
+                {
+                    if (!ProcessFiles(i))
+                    {
+                        somethingWentWrongFlag = true;
+                        errorProcessingFilesMessage += Path.GetFileName(files[i]) + Environment.NewLine;
+                    }
+                }
+
+                if (somethingWentWrongFlag)
+                {
+                    errorProcessingFilesMessage += "and therefore were ignored";
+                    MessageBox.Show(errorProcessingFilesMessage);
+                }
+                else
+                    MessageBox.Show("Done");
+
+                resetEverything();
+            }
+        }
+
+        private bool ValidateForm()
+        {
+            if (string.IsNullOrWhiteSpace(label_outputPath.Text))
+            {
+                MessageBox.Show("you must choose output Path");
+                return false;
+            }
+            if (files == null)
+            {
+                MessageBox.Show("you must choose files to compress");
+                return false;
+            }
+            if (String.IsNullOrWhiteSpace(nmQualityCompressionLevel.Text))
+            {
+                MessageBox.Show("you must choose quality compressionLevel from 0-100");
+                return false;
+            }
+            try
+            {
+                var num = int.Parse(nmQualityCompressionLevel.Text);
+                if (num > 100 || num < 0)
+                {
+                    MessageBox.Show("you must choose quality compressionLevel from 0-100");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("you must choose quality compressionLevel from 0-100");
+                return false;
+            }
+            return true;
+        }
+
+        private void btnSelectOutputPath_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    label_outputPath.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            nmQualityCompressionLevel.Text = "50";
+        }
+    }
+}
